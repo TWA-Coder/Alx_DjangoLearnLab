e8b1afb3eee7d18cb606e4801c9183084c5db850
TWA-Coder <twakaru744@gmail.com>
2025-10-28 06:53:15 +0200

    first endpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -6,3 +6,10 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides the standard actions
+    for the Book model — list, create, retrieve, update, and destroy.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
